a3c7062ca3203cd3d384ad9288d082625e05224d
Степан Савчук <igrok.nikson.230@gmail.com>
2020-05-09 17:47:46 +0300

    Created Dialog.

--- app/src/main/java/com/example/balanceofcash/AddNewRowDialogFragment.java
+++ app/src/main/java/com/example/balanceofcash/AddNewRowDialogFragment.java
@@ -5,18 +5,16 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-
-import androidx.fragment.app.FragmentActivity;
+import android.widget.EditText;
 
 import androidx.fragment.app.DialogFragment;
 
-public class AndroidFragment extends DialogFragment {
+public class AddNewRowDialogFragment extends DialogFragment {
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState){
+    public Dialog onCreateDialog( Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = this.getLayoutInflater();
 
-        builder.setTitle("New");
+        LayoutInflater inflater = getActivity().getLayoutInflater();
 
         builder.setView(inflater.inflate(R.layout.dialog_add, null)).setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
@@ -26,16 +24,9 @@ public class AndroidFragment extends DialogFragment {
         }).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-
+                AddNewRowDialogFragment.this.getDialog().cancel();
             }
         });
         return builder.create();
-    }
-}
-
-class Fragment extends FragmentActivity {
-    public void confirmNew() {
-        DialogFragment newFragment = new AndroidFragment();
-        newFragment.show(getSupportFragmentManager(), "new");
     }
 }
--- app/src/main/java/com/example/balanceofcash/MainActivity.java
+++ app/src/main/java/com/example/balanceofcash/MainActivity.java
@@ -17,16 +17,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
-    Fragment fragment;
-
     Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fragment = new Fragment();
 
         button = findViewById(R.id.buttonAdd);
 
@@ -35,7 +31,9 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG);
-        fragment.confirmNew();
+        Toast.makeText(getApplicationContext(), "Hello World", Toast.LENGTH_LONG).show();
+
+        DialogFragment newFragment = new AddNewRowDialogFragment();
+        newFragment.show(getSupportFragmentManager(), "new");
     }
 }
